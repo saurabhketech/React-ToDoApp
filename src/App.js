156d91a1a92af60a358d83c4f61fde87acbb8a30
upexcel <rahul.excel2011@gmail.com>
2020-02-20 11:28:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './components/dashboard';
+function App() {
+  return (
+    <div className="App">
+        <Dashboard />
+    </div>
+  );
+}
+
+export default App;
